57ed360a49dc2c84b4fbb308c19128456c0b86dd
Skwie <j_groenewoud@hotmail.com>
2024-12-27 13:43:45 +0100

    Add solutions for day 9

---
+++ Day9Part1.ps1
@@ -0,0 +1,46 @@
+﻿$puzzleInput = @'
+
+'@
+
+[array]$lines = $puzzleInput.Split("`n").Trim().ToCharArray()
+
+$fileList = [System.Collections.ArrayList]@()
+$fileIndex = 0
+$counter = 0
+foreach ($number in $lines) {
+    [int]$num = [convert]::ToInt32($number, 10)
+    if ($counter % 2 -eq 0 -or $counter -eq 0) {
+        for ($i = 0; $i -lt $num; $i++) {
+            $null = $fileList.Add("$fileIndex")
+        }
+        $fileIndex++
+    }
+    else {
+        for ($i = 0; $i -lt $num; $i++) {
+            $null = $fileList.Add(".")
+        }
+    }
+    $counter++
+}
+
+for ($i = $fileList.Count - 1; $i -ge 0; $i--) {
+    if ($fileList[$i] -match '\d') {
+        for ($j = 0; $j -lt $fileList.Count; $j++) {
+            if ($fileList[$j] -match '\.' -and $j -lt $i) {
+                $fileList[$j] = $fileList[$i]
+                $fileList[$i] = '.'
+                Break
+            }
+        }
+    }
+}
+
+[int64]$sum = 0
+
+for ($i = 0; $i -lt $fileList.Count; $i++) {
+    if ($fileList[$i] -match '\d') {
+        $sum += [int]$fileList[$i] * $i
+    }
+}
+
+$sum
